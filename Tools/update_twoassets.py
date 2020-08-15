92dbe08f810299b59e341cb0b9e52b527a30f30b
AidenS <aidensoedjarwo@gmail.com>
2020-08-15 14:06:21 +1000

    fix paths

--- Tools/update_twoassets.py
+++ Tools/update_twoassets.py
@@ -7,14 +7,20 @@ def for_context(context):
         for name in files:
             # Compare it with twoassets
             p = os.path.join(root, name)
-            d= f"..\\twoassets\\{context}\\{name}"
+            newPath = p.split("/")[2].split("\\")[1:]
+            newPath = '/'.join(newPath)
+            print(newPath)
+            #newPath = p.strip(f"../Content/{context}")
+            #print(newPath)
+
+            d= f"..\\twoassets\\{context}\\{newPath}"
             try:
                 f1 =os.path.getctime(p) > os.path.getctime(d)
                 if (f1):
-                    print(f"[MODIFIED]: {p}")
+                    print(f"[MODIFIED]: {p}", "New Path:", d)
                     copyfile(p,d)
             except:
-                print(f"[NEW]: {p}")
+                print(f"[NEW]: {p}", d)
                 copyfile(p, d)
            
         
